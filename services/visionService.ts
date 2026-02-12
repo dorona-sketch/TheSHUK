f725066b9ca63b525a1d6ec1d919ce2786be8831
dorona-sketch <doron.a@superplay.co>
2026-02-12 22:33:12 +0200

    Remove vision Service

--- services/visionService.ts
+++
@@ -1,6 +0,0 @@
-
-/**
- * @deprecated This service has been fully superseded by CardRecognitionService and geminiService.
- * Please use CardRecognitionService.identify() for all identification tasks.
- */
-export const visionService = {};
