951f8aa8e37d34d872ffb7df39afb8e90e331e97
Doron <Doron.a@superplay.co>
2026-02-15 22:08:25 +0200

    Update README.md

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.doron.theshuk',
+  appName: 'The Digital Shuk',
+  webDir: 'dist'
+};
+
+export default config;
